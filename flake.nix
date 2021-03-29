71a7b77768c3295b76551928c4de3be57b5c0d1f
Paul d'Hubert <paul@tymate.com>
2021-03-29 08:33:48 +0200

    chore: update flake

--- flake.nix
+++ flake.nix
@@ -9,67 +9,73 @@
   };
 
   outputs = { self, nixpkgs, flake-utils, display-switch, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let pkgs = import nixpkgs { inherit system; };
-      in rec {
-        packages.display-switch = pkgs.rustPlatform.buildRustPackage rec {
-          pname = "display-switch";
-          version = "master";
+    flake-utils.lib.eachDefaultSystem (
+      system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+        in
+          rec {
+            packages.display-switch = pkgs.rustPlatform.buildRustPackage rec {
+              pname = "display-switch";
+              version = "master";
 
-          src = display-switch;
+              src = display-switch;
 
-          cargoSha256 = "sha256-msjbhcKmovGWLlCiqLvZSqwQKYFOfAW15lEuFhqISzI=";
-          doCheck = false;
+              cargoSha256 = "sha256-msjbhcKmovGWLlCiqLvZSqwQKYFOfAW15lEuFhqISzI=";
+              doCheck = false;
 
-          nativeBuildInputs = [ pkgs.pkgconfig ];
+              nativeBuildInputs = [ pkgs.pkgconfig ];
 
-          buildInputs = [ pkgs.libudev ];
-        };
-
-        overlay = final: prev: { display-switch = packages.display-switch; };
-
-        nixosModule = { config, lib, pkgs, ... }:
-          let
-            inherit (lib) mkEnableOption mkOption types mkIf generators;
-
-            monitorOptions = {
-              type = types.submodule;
-              options = {
-                monitorId = mkOption {
-                  type = types.either types.str types.int;
-                  description = "ID or substring of the monitor name";
-                };
-
-                onUsbConnect = mkOption {
-                  type = types.nullOr types.str;
-                  description =
-                    "DCC/IC output to enable when the usb device is connected";
-                };
-
-                onUsbDisconnect = mkOption {
-                  type = types.nullOr types.str;
-                  description =
-                    "DCC/IC output to enable when the usb device is disconnected";
-                };
-              };
+              buildInputs = [ pkgs.libudev ];
             };
 
-            mkMonitorConfig = name: cfg: ''
-              [${name}]
-              monitor_id = "${builtins.toString cfg.usbDevice}"
-              ${if cfg.onUsbConnect then
-                ''on_usb_connect = "${cfg.onUsbConnect}"''
-              else
-                ""}
+            overlay = final: prev: { display-switch = packages.display-switch; };
+          }
+    ) // {
+      nixosModule = { config, lib, pkgs, ... }:
+        let
+          inherit (lib) mkEnableOption mkOption types mkIf generators;
 
-              ${if cfg.onUsbDisconnect then
-                ''on_usb_disconnect = "${cfg.onUsbDisconnect}"''
-              else
-                ""}
-            '';
+          monitorOptions = {
+            options = {
+              monitorId = mkOption {
+                type = types.either types.str types.int;
+                description = "ID or substring of the monitor name";
+              };
 
-            cfg = config.services.display-switch;
-          in {
+              onUsbConnect = mkOption {
+                type = types.nullOr types.str;
+                default = null;
+                description =
+                  "DCC/IC output to enable when the usb device is connected";
+              };
+
+              onUsbDisconnect = mkOption {
+                type = types.nullOr types.str;
+                default = null;
+                description =
+                  "DCC/IC output to enable when the usb device is disconnected";
+              };
+            };
+          };
+
+          mkMonitorConfig = name: monitor: ''
+            [${name}]
+            monitor_id = "${builtins.toString monitor.monitorId}"
+            ${if monitor.onUsbConnect != null then
+            ''on_usb_connect = "${monitor.onUsbConnect}"''
+          else
+            ""}
+
+            ${if monitor.onUsbDisconnect != null then
+            ''on_usb_disconnect = "${monitor.onUsbDisconnect}"''
+          else
+            ""}
+          '';
+
+          cfg = config.services.display-switch;
+        in
+          {
             options = {
               services.display-switch = {
                 enable = mkEnableOption "Enable display-switch service";
@@ -82,25 +88,27 @@
 
                 onUsbConnect = mkOption {
                   type = types.nullOr types.str;
+                  default = null;
                   description =
                     "DCC/IC output to enable when the usb device is connected";
                 };
 
                 onUsbDisconnect = mkOption {
                   type = types.nullOr types.str;
+                  default = null;
                   description =
                     "DCC/IC output to enable when the usb device is disconnected";
                 };
 
                 monitors = mkOption {
-                  type = types.attrsOf monitorOptions;
+                  type = types.attrsOf (types.submodule monitorOptions);
                   description = "Monitor specific configuration overrides";
                 };
               };
             };
 
             config = mkIf cfg.enable {
-              nixpkgs.overlays = [ overlay ];
+              nixpkgs.overlays = [ self.overlay."${config.nixpkgs.system}" ];
 
               boot.kernelModules = [ "i2c_dev" ];
 
@@ -111,35 +119,31 @@
               environment.etc."display-switch/display-switch.ini".text = ''
                 usb_device = "${cfg.usbDevice}"
 
-                ${if cfg.onUsbConnect then
-                  ''on_usb_connect = "${cfg.onUsbConnect}"''
-                else
-                  ""}
+                ${if cfg.onUsbConnect != null then
+                ''on_usb_connect = "${cfg.onUsbConnect}"''
+              else
+                ""}
 
-                ${if cfg.onUsbDisconnect then
-                  ''on_usb_disconnect = "${cfg.onUsbDisconnect}"''
-                else
-                  ""}
+                ${if cfg.onUsbDisconnect != null then
+                ''on_usb_disconnect = "${cfg.onUsbDisconnect}"''
+              else
+                ""}
 
-                ${builtins.concatStringsSep "\n"
-                (builtins.mapAttrsToList mkMonitorConfig cfg.monitors)}
+                ${lib.concatStringsSep "\n"
+                (lib.mapAttrsToList mkMonitorConfig cfg.monitors)}
               '';
 
-              systemd.user.services.display-switch = {
-                Unit = {
-                  Description = "display-switch";
-                  After = [ "graphical-session-pre.target" ];
-                  PartOf = [ "graphical-session.target" ];
-                };
+              systemd.services.display-switch = {
+                description = "display-switch";
 
-                Install = { WantedBy = [ "graphical-session.target" ]; };
+                partOf = [ "graphical.target" ];
+                wantedBy = [ "graphical.target" ];
 
-                Service = {
-                  Environment = [ "XDG_CONFIG_HOME=/etc" ];
-                  ExecStart = "${pkgs.display-switch}/bin/display_switch";
-                };
+                environment = { XDG_CONFIG_HOME = "/etc"; };
+
+                script = "${pkgs.display-switch}/bin/display_switch";
               };
             };
           };
-      });
+    };
 }
